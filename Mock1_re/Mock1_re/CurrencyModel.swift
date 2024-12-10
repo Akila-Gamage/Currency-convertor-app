2c35b468a2263eeb623ebae34f7aa7a8a53b6db0
Akila Budhvin Gamage <95086925+Akila-Gamage@users.noreply.github.com>
2024-12-10 14:05:55 +0530

    Initial commit

---
+++ Mock1_re/Mock1_re/CurrencyModel.swift
@@ -0,0 +1,16 @@
+//
+//  CurrencyModel.swift
+//  Mock1_re
+//
+//  Created by Akila Gamage on 2024-12-05.
+//
+
+import Foundation
+
+enum Currencies: String, CaseIterable {
+    case usd = "USD"
+    case gbp = "GBP"
+    case aud = "AUD"
+    case cad = "CAD"
+    case eur = "EUR"
+}
